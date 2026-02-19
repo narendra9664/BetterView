7ed5ebf6b04b0bc6f853459305a1ec3992b6efba
Narendra <chittorgarhvlog00@gmail.com>
2026-02-19 07:15:43 +0530

    implement homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar"
+import {ArrowRight, Clock, Layers} from "lucide-react";
+import Button from "../../components/ui/Button";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -7,11 +9,95 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+function ArrowUpRigh(props: { size: number }) {
+    return null;
+}
+
 export default function Home() {
   return (
       <div className="home">
         <Navbar />
-        <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+        <section className="hero">
+            <div className="announce">
+                <div className="dot">
+                    <div className="pulse"></div>
+                </div>
+
+                <p>Introducing BetterView 2.0</p>
+            </div>
+
+            <h1>Build beatiful spaces at the speed of thought with BetterView</h1>
+
+            <p className="subtitle">
+                BetterView is an AI-first design environment that helps you visualize, render, and ship architectural projects faster that ever.
+            </p>
+
+            <div className="actions">
+                <a href="#upload" className="cta">
+                    start building <ArrowRight className="icon"/>
+                </a>
+
+                <Button variant="outline" size="lg" className="demo">
+                    Watch Demo
+                </Button>
+            </div>
+
+            <div id="upload" className="upload-shell">
+                <div className="grid-overlay"/>
+
+                <div className="upload-card">
+                    <div className="upload-head">
+                        <div className="upload-icon">
+                            <Layers className="icon"/>
+                        </div>
+
+                        <h3>Upload your floor plan</h3>
+                        <p>Supports JPG, PNG, Formate up to 10MB</p>
+                    </div>
+
+                    <p>Upload images</p>
+                </div>
+            </div>
+        </section>
+
+        <section className="projects">
+            <div className="section-inner">
+                <div className="section-head">
+                    <div className="copy">
+                        <h2>Projects</h2>
+                        <p>Your latest work and shared community projects, all in one place</p>
+                    </div>
+                </div>
+
+                <div className="projects-grid">
+                    <div className="projects-card group">
+                        <div className="preview">
+                            <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                             alt="project"/>
+
+                            <div className="badge">
+                                <span>Community</span>
+                            </div>
+                        </div>
+
+                        <div className="card-body">
+                            <div>
+                                <h3>Project manhattan</h3>
+
+                                <div className="meta">
+                                    <Clock size={12}/>
+                                    <span>{new Date('02,01,2027'). toLocaleDateString()}</span>
+                                    <span>By ravi</span>
+                                </div>
+                            </div>
+                            <div className="arrow">
+                                <ArrowUpRigh size={18}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
       </div>
   )
 
